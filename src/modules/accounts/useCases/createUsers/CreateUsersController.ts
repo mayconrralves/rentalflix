8925c019de8a97e51be07a2594bd4639acc17cdf
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-08-06 10:17:04 -0300

    Alter table of user

--- src/modules/accounts/useCases/createUsers/CreateUsersController.ts
+++ src/modules/accounts/useCases/createUsers/CreateUsersController.ts
@@ -5,11 +5,10 @@ import { CreateUsersUseCase } from './CreateUsersUseCase'
 
 class CreateUsersController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { name, username, email, password, driver_license } = request.body
+        const { name, email, password, driver_license } = request.body
         const createUsersUserCase = container.resolve(CreateUsersUseCase)
         await createUsersUserCase.execute({
             name,
-            username,
             email,
             password,
             driver_license,
