123295abaf2e6ca39fd71c7a30cd7945a3d3b717
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-08-16 00:17:38 -0300

    'Teste de criação de categoria': completed

---
+++ src/modules/cars/repositories/in-memories/CategoryRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../entities/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../implements/ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+    categories: Category[] =[];
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find(category => category.name === name);
+        return category;
+    }
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = new Category();
+        Object.assign(category, {
+            name, description
+        });
+        this.categories.push(category);
+    }
+    list(): Promise<Category[]> {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export {CategoriesRepositoryInMemory }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 import { CreateUsersController } from '../modules/accounts/useCases/createUsers/CreateUsersController'
-
+import { UpdateUserAvatarController } from '../modules/cars/useCases/updateUserAvatarUseCase.ts/UpdateUserAvatarController'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 const usersRoutes = Router()
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 const createUsersController = new CreateUsersController()
+const updateUserAvatarController = new UpdateUserAvatarController()
 usersRoutes.post('/', createUsersController.handle)
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"),updateUserAvatarController.handle)
 
 export { usersRoutes }
