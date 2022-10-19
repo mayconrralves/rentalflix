3b740fb9c07b78878d854165fb923774d8c5becf
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-10-19 11:09:23 -0300

    database dist configure

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,4 @@
-import { Connection, createConnection, getConnectionOptions } from 'typeorm'
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 // interface IOptions {
 //     host: string
@@ -12,13 +12,15 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 //     })
 // })
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
-            database: process.env.NODE_ENV === "test" ? "rentx_test" :defaultOptions.database,
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? 'rentx_test'
+                    : defaultOptions.database,
         })
     );
-}
+};
