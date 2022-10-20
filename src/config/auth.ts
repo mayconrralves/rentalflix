73859305980dcfe2ef2573bf004d24b7cd1f6eea
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-10-20 11:27:00 -0300

    change expires token to 2 days

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,7 +2,6 @@ export default {
     expires_in_token: '15d',
     secret_token: 'qualquercoisa',
     secret_refresh_token: 'qualquercoisarefresh',
-    expires_in_refresh_token: '30d',
-    expires_in_refresh_token_days: 30
-
-}
+    expires_in_refresh_token: '2d',
+    expires_in_refresh_token_days: 2,
+};
