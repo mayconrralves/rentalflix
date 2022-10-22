47eeec60f0deb5a8798ef8865f59068f30ab9805
mayconrralves@yahoo.com.br <mayconrralves@yahoo.com.br>
2022-10-22 12:19:31 -0300

    Added cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 
 import { AppError } from '../../errors/AppError';
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+app.use(cors());
 app.use(router);
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
